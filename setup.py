8fd32c307d74c6f472ce79c2c09d7c5fbe6411f3
Michael Howitz <mh@gocept.com>
2009-05-18 17:03:16 +0000

    Preparing release 1.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.browser',
-    version = '1.2dev',
+    version = '1.2',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Shared Zope Toolkit browser components',
