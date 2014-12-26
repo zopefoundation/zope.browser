7745e11c2167a106eb3499e05b18df11ec1f5919
Tres Seaver <tseaver@palladion.com>
2014-12-26 14:16:44 -0500

    Prep 2.1.0 release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-2.1.0 (unreleased)
+2.1.0 (2014-12-26)
 ------------------
 
 - Add support for Python 3.4.
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.browser',
-    version='2.1.0.dev0',
+    version='2.1.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Shared Zope Toolkit browser components',
