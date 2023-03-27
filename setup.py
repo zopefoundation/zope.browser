2f21f250e413089e67f1c2c57561820f2fa03ab3
Michael Howitz <mh@gocept.com>
2023-03-27 10:42:08 +0200

    Back to development: 3.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name='zope.browser',
-    version='3.0',
+    version='3.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Shared Zope Toolkit browser components',
